7d50d800d0c00c748c26ee51e5565620cd9fd89c
Your_Void <my.vo1d.edge@gmail.com>
2023-10-27 19:10:56 +0300

    Haffman_Code

--- Lab№1_Huffman_code.py
+++ Lab№1_Huffman_code.py
@@ -7,3 +7,16 @@ def count_frequencies(text):
         else:
             freq_dict[char] = 1
     return freq_dict
+
+
+#def build_huffman_tree(freq_dict):
+    
+def huffman_code(input_file, output_file):
+    freq_dict = count_frequencies(input_file)
+#def huffman_decode:
+    
+if __name__ == "__main__":
+    code_file = "code_file.txt"
+    decode_file = "decode_file.txt"
+    
+    
