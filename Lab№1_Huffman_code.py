6bcce4f740a8db945aec8377a491715d4d97600f
Your_Void <my.vo1d.edge@gmail.com>
2023-10-28 16:56:08 +0300

    Huffman_code

--- Lab№1_Huffman_code.py
+++ Lab№1_Huffman_code.py
@@ -9,7 +9,19 @@ def count_frequencies(text):
     return freq_dict
 
 
-#def build_huffman_tree(freq_dict):
+def build_huffman_tree(freq_dict):
+    nodes = [(char, freq, []) for char, freq in freq_dict.items()]
+
+    while len(nodes) > 1:
+        nodes.sort(key=lambda x: x[1])
+        left = nodes.pop(0)
+        right = nodes.pop(0)
+        merged_node = (None, left[1] + right[1], [left, right])
+        nodes.append(merged_node)
+
+    return nodes[0]
+
+
     
 def huffman_code(input_file, output_file):
     freq_dict = count_frequencies(input_file)
