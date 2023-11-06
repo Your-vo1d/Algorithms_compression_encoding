17708fe42e20b45d02723df334f78fae2d0a8401
Your_Void <my.vo1d.edge@gmail.com>
2023-11-06 15:39:37 +0300

    Code_Huffman

--- Lab№1_Huffman_code.py
+++ Lab№1_Huffman_code.py
@@ -9,11 +9,11 @@ def build_huffman_tree(symbols_freq):
     while len(heap) > 1:
         low = heapq.heappop(heap)
         high = heapq.heappop(heap)
-        for pair in lo[1:]:
+        for pair in low[1:]:
             pair[1] = '0' + pair[1]
-        for pair in hi[1:]:
+        for pair in high[1:]:
             pair[1] = '1' + pair[1]
-        heapq.heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
+        heapq.heappush(heap, [low[0] + high[0]] + low[1:] + high[1:])
 
     return heap[0]
 
